546c9b1b6adca188bf5ccb7b6ab68a5d8c3b5e51
BerryZSZ <zhangsz@mail.sustc.edu.cn>
2017-08-14 15:31:25 +0800

    adding

---
+++ Mod/Test/TestCreatTXT.lua
@@ -0,0 +1,27 @@
+--[[
+Title: imP test file
+Author(s): BerryZSZ
+Date: 2017/8/14
+Desc: 
+use the lib:
+------------------------------------------------------------
+NPL.load("(gl)Mod/Test/TestCreatTXT.lua");
+------------------------------------------------------------
+]]
+NPL.load("(gl)Mod/ImagesTo3Dmodel/imP.lua");
+
+local ImageFilename = "Mod/ImagesTo3Dmodel/demo-data/church3/2.jpg";
+local TxtFilename1 = "D:/University/SOC2017/LuaCode/lena.txt";
+local TxtFilename2 = "Mod/ImagesTo3Dmodel/demo-data/church3/2.txt";
+local TxtFilename3 = "D:/University/SOC2017/ParaCraftSDK-master/ParaCraftSDK-master/_mod/ImagesTo3Dmodel/Mod/ImagesTo3Dmodel/demo-data/church3/2.txt";
+
+local Im = imP.imread(ImageFilename);
+local I = imP.rgb2gray(Im);
+
+local I_O = imP.tensor.imresize(I,0.5);
+LOG.std(nil,"debug","TestCreatTXT","-------Start 1----------");
+imP.CreatTXT(I_O, TxtFilename1);
+LOG.std(nil,"debug","TestCreatTXT","-------Start 2----------");
+imP.CreatTXT(I_O, TxtFilename2);
+LOG.std(nil,"debug","TestCreatTXT","-------Start 3----------");
+imP.CreatTXT(I_O, TxtFilename3);
