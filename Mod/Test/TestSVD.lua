cc44c071e3d0a94a28368bad15ed677345bee082
BerryZSZ <zhangsz@mail.sustc.edu.cn>
2017-09-04 14:50:56 +0800

    1

--- Mod/Test/TestSVD.lua
+++ Mod/Test/TestSVD.lua
@@ -13,6 +13,8 @@ NPL.load("(gl)Mod/ImagesTo3Dmodel/imP.lua",true);
 NPL.load("(gl)Mod/ImagesTo3Dmodel/SVD.lua",true);
 
 local ArrayShow = imP.tensor.ArrayShow;
+local MatrixMultiple = imP.tensor.MatrixMultiple;
+local transposition = imP.tensor.transposition;
 
 local DO_SVD = SVD.DO_SVD;
 
@@ -37,6 +39,12 @@ print("SA")
 ArrayShow(SA)
 print("VA")
 ArrayShow(VA)
+print("UA*UA'")
+ArrayShow(MatrixMultiple(UA, transposition(UA)));
+print("VA*VA'")
+ArrayShow(MatrixMultiple(VA, transposition(VA)));
+print("UA*SA*VA'")
+ArrayShow(MatrixMultiple(MatrixMultiple(UA, SA), transposition(VA)));
 print("--------------- B -------------------");
 print("UB")
 ArrayShow(UB)
@@ -44,6 +52,12 @@ print("SB")
 ArrayShow(SB)
 print("VB")
 ArrayShow(VB)
+print("UB*UB'")
+ArrayShow(MatrixMultiple(UB, transposition(UB)));
+print("VB*VB'")
+ArrayShow(MatrixMultiple(VB, transposition(VB)));
+print("UB*SB*VB'")
+ArrayShow(MatrixMultiple(MatrixMultiple(UB, SB), transposition(VB)));
 print("--------------- C -------------------");
 print("UC")
 ArrayShow(UC)
@@ -51,5 +65,11 @@ print("SC")
 ArrayShow(SC)
 print("VC")
 ArrayShow(VC)
+print("UC*UC'")
+ArrayShow(MatrixMultiple(UC, transposition(UC)));
+print("VC*VC'")
+ArrayShow(MatrixMultiple(VC, transposition(VC)));
+print("UC*SC*VC'")
+ArrayShow(MatrixMultiple(MatrixMultiple(UC, SC), transposition(VC)));
 
 
